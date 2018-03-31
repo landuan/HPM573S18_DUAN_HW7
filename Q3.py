1707a6882df4c2bbc71f820e229eaf9dfd291162
Lan Duan <lan.duan@yale.edu>
2018-03-31 08:26:13 -0400

    yes

--- Q3.py
+++ Q3.py
@@ -1,13 +1,13 @@
 import scipy.stats as Stat
 
-##50% of the patients in our simulated model survived beyond 5 years:
-percentage = 0.5
+# 50% of the patients in the model survived 5yr +
+Percent = 0.5
 
-#a clinical study reports 400 of 573 participants survived:
-kstudy=400
-nstudy=573
+# a clinical study reports 400 of 573 participants survived:
+survived =400
+total_studied=573
 
 #caculate the likelihood:
-weight=Stat.binom.pmf(k=kstudy,n=nstudy,p=percentage)
+weight=Stat.binom.pmf(k=survived,n=total_studied,p=Percent)
 
 print(weight)
